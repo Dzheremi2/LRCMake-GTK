5eb3e0541e607bcbbf87d7c07c4bfd4028411cfa
Dzheremi-belpois <chichvarov1315@gmail.com>
2024-11-14 00:43:07 +0300

    feat: added keybindings help overlay

--- src/components/syncLine.py
+++ src/components/syncLine.py
@@ -8,7 +8,7 @@ class syncLine(Adw.EntryRow):
     __gtype_name__ = 'syncLine'
 
     def __init__(self, *args):
-        super().__init__(*args)
+        super().__init__()
         self.focus_controller = Gtk.EventControllerFocus()
         self.focus_controller.connect('enter', self.update_selected_row)
         self.add_controller(self.focus_controller)
